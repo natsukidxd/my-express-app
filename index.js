4013f7f78500f2e6ef2c0c87c9970d5e01db3367
natsukidxd <natsukid123@gmail.com>
2026-02-28 19:25:26 +0800

    feat: add static file serving and update routes in index.js

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Serve static files from the 'public folder'
+app.use(express.static('public'))
+
 // Define route for the home page
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Receieved: ${JSON.stringify(data)}`);
 });
 
 // Start the server
