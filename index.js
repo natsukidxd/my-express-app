198c7bf6b7c271b60bc7ee4bababd491b3285545
natsukidxd <natsukid123@gmail.com>
2026-03-03 12:44:06 +0800

    refactor: organize middleware and routes in index.js for better readability

--- index.js
+++ index.js
@@ -2,26 +2,49 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'public folder'
-app.use(express.static('public'))
+// Middleware 
+app.use(express.static('public'));
+app.use(express.json());
 
-// Define route for the home page
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Us');
+  res.send('About Us');
 });
 
-app.use(express.json());
-
 app.post('/submit', (req, res) => {
-    const data = req.body;
-    res.send(`Receieved: ${JSON.stringify(data)}`);
+  const data = req.body;
+  res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+  const newItem = req.body.item;
+
+  items.push(newItem);
+  res.json(items);
+});
+
+// Error handler (last)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
